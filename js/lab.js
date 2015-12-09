3fa0f667d3c63a154a5f475e88ad13a44e16cbe8
unknown <you@example.com>
2015-12-08 20:42:35 -0500

    added code for spock and lizard

--- js/lab.js
+++ js/lab.js
@@ -4,52 +4,91 @@ $(document).ready(function() {
   var compScore = 0
 
   $(".shoot").on("click", function() {
-      var choiceRPS = ['rock', 'paper', 'scissor'];
+      var choiceRPS = ['rock', 'paper', 'scissor','lizard','spock'];
       var ranNum = Math.floor(Math.random() * choiceRPS.length);
       var compChoice = choiceRPS[ranNum];
       var userChoice = this.id;
       round++;
       $("#round").html(round);
       var compChoice = choiceRPS[ranNum];
-      console.log(compChoice);
       console.log(userChoice);
+      console.log(compChoice);
+      console.log(round);
 
       if (compChoice == userChoice) {
         console.log("it's a tie");
       };
 
       if (userChoice === "rock") {
-        if (compChoice === "scissor") {
+        if (compChoice === "lizard" || compChoice === "scissor") {
           yourScore++;
           $("#yourScore").html(yourScore);
         } else {
-          if (compChoice === "paper") {
+          if (compChoice === "paper" || compChoice === "spock") {
             compScore++;
             $("#computerScore").html(compScore);
           }
         }
       };
       if (userChoice === "paper") {
-        if (compChoice === "rock") {
+        if (compChoice === "rock" || compChoice === "spock") {
           yourScore++;
           $("#yourScore").html(yourScore);
         } else {
-          if (compChoice === "scissor") {
+          if (compChoice === "scissor" || compChoice === "lizard") {
             compScore++;
             $("#computerScore").html(compScore);
           }
         }
       };
       if (userChoice === "scissor") {
-        if (compChoice === "paper") {
+        if (compChoice === "paper" || compChoice === "lizard") {
           yourScore++;
           $("#yourScore").html(yourScore);
         } else {
-          if (compChoice === "rock") {
+          if (compChoice === "rock" || compChoice === "spock") {
+            compScore++;
+            $("#computerScore").html(compScore);
+          }
+        }
+      };      
+      if (userChoice === "lizard") {
+        if (compChoice === "spock" || compChoice === "paper") {
+          yourScore++;
+          $("#yourScore").html(yourScore);
+        } else {
+          if (compChoice === "rock" || compChoice === "scissor") {
+            compScore++;
+            $("#computerScore").html(compScore);
+          }
+        }
+      };      
+      if (userChoice === "spock") {
+        if (compChoice === "rock" || compChoice === "scissor") {
+          yourScore++;
+          $("#yourScore").html(yourScore);
+        } else {
+          if (compChoice === "lizard" || compChoice === "paper") {
             compScore++;
             $("#computerScore").html(compScore);
           }
         }
       };
-  });
+      if (round === 5) {
+        round -=5;
+        if (yourScore>compScore) {
+          $('#win').modal({
+            keyboard: false
+          });
+        } else if (yourScore<compScore) {
+          $('#lose').modal({
+            keyboard: false
+          });
+        } else if (yourScore==compScore) {
+            $('#tie').modal({
+            keyboard: false
+        });
+      };
+  };
+});
 });
