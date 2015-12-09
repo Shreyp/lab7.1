9250ba63191ea07afbd891e0304f9f50952b0d8f
unknown <you@example.com>
2015-12-08 19:25:47 -0500

    added logic for scoring

--- js/lab.js
+++ js/lab.js
@@ -1,19 +1,55 @@
 $(document).ready(function() {
-    var round = 0
+  var round = 0
+  var yourScore = 0
+  var compScore = 0
 
-    $("#rock", "#paper", "#scissor").on("click", function(){
-        var userChoice = $(this).data("choice");
-    })
+  $(".shoot").on("click", function() {
+      var choiceRPS = ['rock', 'paper', 'scissor'];
+      var ranNum = Math.floor(Math.random() * choiceRPS.length);
+      var compChoice = choiceRPS[ranNum];
+      var userChoice = this.id;
+      round++;
+      $("#round").html(round);
+      var compChoice = choiceRPS[ranNum];
+      console.log(compChoice);
+      console.log(userChoice);
 
-    $(".shoot").on("click", function() {
-        var choiceRPS = ['rock','paper','scissors'];
-        var ranNum = Math.floor(Math.random() * choiceRPS.length); 
-        round ++;
-        $("#round").html(round);
-        var compChoice = choiceRPS[ranNum];
-        console.log(compChoice);
-    });
+      if (compChoice == userChoice) {
+        console.log("it's a tie");
+      };
 
-
-
+      if (userChoice === "rock") {
+        if (compChoice === "scissor") {
+          yourScore++;
+          $("#yourScore").html(yourScore);
+        } else {
+          if (compChoice === "paper") {
+            compScore++;
+            $("#computerScore").html(compScore);
+          }
+        }
+      };
+      if (userChoice === "paper") {
+        if (compChoice === "rock") {
+          yourScore++;
+          $("#yourScore").html(yourScore);
+        } else {
+          if (compChoice === "scissor") {
+            compScore++;
+            $("#computerScore").html(compScore);
+          }
+        }
+      };
+      if (userChoice === "scissor") {
+        if (compChoice === "paper") {
+          yourScore++;
+          $("#yourScore").html(yourScore);
+        } else {
+          if (compChoice === "rock") {
+            compScore++;
+            $("#computerScore").html(compScore);
+          }
+        }
+      };
+  });
 });
